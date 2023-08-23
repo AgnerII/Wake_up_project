bcc46c73cf9a8aeb0db80c4c9b92fdaf93348996
AgnerII <noah.0410@hotmail.com>
2023-08-23 10:37:07 +0200

    Initial

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    private double result;
+
+
+    public double add(double x, double y){
+        result = y+x;
+        return result;
+    }
+
+
+
 }
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,4 +1,22 @@
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+    Calculator calculator;
+    @BeforeEach
+            void setUp(){
+        calculator = new Calculator();
+    }
+    @Test
+    void testAdd(){
+        assertEquals(10,calculator.add(6,4));
+    }
+    @RepeatedTest(1)
+    void testAddWithNegative(){
+        assertEquals(7,calculator.add(-5,12));
+    }
+
 }
